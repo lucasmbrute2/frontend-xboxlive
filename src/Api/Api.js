b9d2d3162548619bf468559a572d23f31cb2cfd7
Lucas <lucasmbrute614@gmail.com>
2021-10-19 02:04:05 -0300

    chore(Api): fixing method's name in Api call

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -3,7 +3,7 @@ export const Api={
     
     getAll:(path)=> fetch(Api.baseUrl+path),
     
-    getById:(path,id)=> fetch(Api.baseUrl+path),
+    getById:(path,id)=> fetch(`${Api.baseUrl}${path}/${id}`),
     
     post:(path,body)=> {
         return fetch(Api.baseUrl+path,{
@@ -14,7 +14,7 @@ export const Api={
     })},
     
     update:(path,body,id)=>{
-        return fetch(Api.baseUrl+path+"/update"+id,{
+        return fetch(Api.baseUrl+path+"/update/"+id,{
         method:"PUT",
         headers: new Headers({
             "Content-type": "application/json"
@@ -22,7 +22,7 @@ export const Api={
         body:JSON.stringify(body)
     })},
     delete: (path,body,id)=> {
-        return fetch(Api.baseUrl+path+"/delete"+id,{
+        return fetch(Api.baseUrl+path+"/delete/"+id,{
         method: "DELETE",
         headers: new Headers({
             "Content-type":"application/json"
