87484af59268122e463fcca28171ead529888e95
Lucas <lucasmbrute614@gmail.com>
2021-10-18 22:32:38 -0300

    feat(Api): implement method Get, Update ,Delete

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,5 +1,21 @@
 export const Api={
-    baseUrl: "http://localhost:3000/game",
+    baseUrl: "http://localhost:3000/",
     
-    getAll:()=> fetch(Api.baseUrl)
+    getAll:(path)=> fetch(Api.baseUrl+path),
+    getById:(path,id)=> fetch(Api.baseUrl+path),
+    update:(path,body,id)=>fetch(Api.baseUrl+path+"/add"+id,{
+        method:"POST",
+        headers: new Headers({
+            "Content-type": "application/json"
+        }),
+        body:JSON.stringify(body)
+    }),
+    delete: (path,body,id)=> fetch(Api.baseUrl+path+"/delete"+id,{
+        method: "DELETE",
+        headers: new Headers({
+            "Content-type":"application/json"
+        }),
+        body:JSON.stringify(body)
+    })
+
 }
