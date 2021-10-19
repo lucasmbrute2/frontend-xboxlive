1ce9fbd8dc0c111c379954514581c24c8d5590b9
Lucas <lucasmbrute614@gmail.com>
2021-10-18 22:37:30 -0300

    feat(Api): implement method POST

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -2,20 +2,32 @@ export const Api={
     baseUrl: "http://localhost:3000/",
     
     getAll:(path)=> fetch(Api.baseUrl+path),
+    
     getById:(path,id)=> fetch(Api.baseUrl+path),
-    update:(path,body,id)=>fetch(Api.baseUrl+path+"/add"+id,{
+    
+    post:(path,body)=> {
+        return fetch(Api.baseUrl+path,{
         method:"POST",
+        headers: new Headers({       
+        }),
+        body: JSON.stringify(body)
+    })},
+    
+    update:(path,body,id)=>{
+        return fetch(Api.baseUrl+path+"/update"+id,{
+        method:"PUT",
         headers: new Headers({
             "Content-type": "application/json"
         }),
         body:JSON.stringify(body)
-    }),
-    delete: (path,body,id)=> fetch(Api.baseUrl+path+"/delete"+id,{
+    })},
+    delete: (path,body,id)=> {
+        return fetch(Api.baseUrl+path+"/delete"+id,{
         method: "DELETE",
         headers: new Headers({
             "Content-type":"application/json"
         }),
         body:JSON.stringify(body)
-    })
+    })}
 
 }
