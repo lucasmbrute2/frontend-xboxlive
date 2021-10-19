0e87684b4e8dc6083df9f161bc33ffb3144b0a1f
Lucas <lucasmbrute614@gmail.com>
2021-10-18 21:12:46 -0300

    feat(src): app structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { Switch, Route } from "react-router-dom"
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/pages/Home/Home';
 
 function App() {
   return (
+    
     <div className="App">
-        Hello
+      <Header/>
+        <Switch>
+          <Route path='/' exact={true} component={Home}/>
+        </Switch>
+      <Footer/>
     </div>
   );
 }
