4d963c43f86395bbe86d698b2c7b0584a9d91eb4
Lucas <lucasmbrute614@gmail.com>
2021-10-20 14:09:23 -0300

    feat(GuardedRoute): create a component to authenticate routes

---
+++ src/components/GuardedRoute/GuardedRoute.js
@@ -0,0 +1,11 @@
+import { Redirect, Route } from "react-router";
+
+export default function GuardedRoute({ component: Component, ...rest }){
+    const auth = true;
+    return(
+    <Route { ...rest } render={props=> 
+        auth ===true ? <Component {...props} />: <Redirect to='/user/add'/>
+    }/>
+   
+    )
+}
