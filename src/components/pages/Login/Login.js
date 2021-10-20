b49fc9134e73b97544edab819746e418805b6092
Lucas <lucasmbrute614@gmail.com>
2021-10-20 15:08:47 -0300

    feat(login): create login page

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -1,3 +1,52 @@
-export default function Login(){
-    return <div>Login</div>
+import { Api } from "../../../Api/Api"
+
+export default function Login(props){
+    
+    const HandleSubmit = async (e)=>{
+        e.preventDefault()
+        
+        const email = e.target.email.value
+        const password = e.target.password.value
+        
+        const payload = {
+            email,
+            password
+        }
+    
+        const response = await Api.post('login',payload)
+        const body = await response.json()
+        console.log(body)
+        
+        if(response.status===200){
+            const accessToken = body.accessToken
+            
+            localStorage.setItem("JWT",accessToken)
+            console.log({ accessToken })
+            props.history.push('/')
+        
+        }else{
+            throw new Error()
+        }
+    }
+    
+    return(
+        <div>
+            <form onSubmit={HandleSubmit}>
+
+                <div>
+                    <input name='email' placeholder='Email'></input>
+                </div>
+
+                <div>
+                    <input name='password' type='password' placeholder='Senha'></input> 
+                </div>
+
+                <div>
+                    <input type='submit' value='Login'></input>
+                </div>
+            
+            </form>
+        </div>
+        
+    )    
 }
