1f0f20a76cb80a3ce5f07ce2f619d6c7ce3d66ba
Lucas <lucasmbrute614@gmail.com>
2021-10-20 14:39:06 -0300

    .

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import CreateUser from './components/pages/CreateUser/CreateUser';
 import DeleteUser from './components/pages/DeleteUser/DeleteUser';
 import UpdateUser from './components/pages/UpdateUser/UpdateUser';
 import GameView from './components/pages/GameView/GameView';
+import GuardedRoute from './components/GuardedRoute/GuardedRoute';
+import Login from './components/pages/Login/Login';
 
 function App() {
   return (
     
     <div className="App">
         <Switch>
-          <Route path='/' exact={true} component={Home}/>
+          <GuardedRoute path='/' exact={true} component={Home}/>
+          <Route path='login' component={Login}/>
           <Route path='/user/add' component={CreateUser}/>
           <Route path='/user/delete/:id' component={DeleteUser}/>
           <Route path='/user/update/:id'component={UpdateUser}/>         
--- src/components/GuardedRoute/GuardedRoute.js
+++ src/components/GuardedRoute/GuardedRoute.js
@@ -1,11 +1,11 @@
 import { Redirect, Route } from "react-router";
 
 export default function GuardedRoute({ component: Component, ...rest }){
-    const auth = true;
+    const auth = Boolean(localStorage.getItem("JWT"));// checa a chave JWT no local storage
     return(
     <Route { ...rest } render={props=> 
         auth ===true ? <Component {...props} />: <Redirect to='/user/add'/>
     }/>
    
     )
-}
+} 
