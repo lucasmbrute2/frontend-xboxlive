dbb8b8daab91bf69e66d3f59e23af078e5cb31b1
Lucas <lucasmbrute614@gmail.com>
2021-10-21 00:53:57 -0300

    feat(GameView): implement page

--- src/components/pages/GameView/GameView.js
+++ src/components/pages/GameView/GameView.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { Api } from "../../../Api/Api";
-
+import "./GameView.scss"
+import { Link } from "react-router-dom"
 const GameView = (props)=> {
     let { id } = props.match.params
     const [gameId,setGameId] = useState({}) 
@@ -13,10 +14,28 @@ const GameView = (props)=> {
         }
         LoadGameUnique()       
     },[id])
+    const today = new Date()
+    const date = new Intl.DateTimeFormat('pt-BR',{hour:"numeric",minute:"numeric"}).format(today)
     return(
-        <div>
-            <p></p>
-            <img src={gameId.cover} alt={gameId.name}/>
+        <div className='view'>
+            <div className='view-header'>
+                <Link to='/'><img src='https://images.vexels.com/media/users/3/142890/isolated/preview/4ea2d7c4bf3cad23a4f18ee58752deb8-high-tech-rings-logo.png'/> </Link>
+                <p className='view-p'>{date}</p>
+            </div>
+            
+            <div className='view-game'>
+                <div className='view-game-container-img'>
+                    <img src={gameId.cover} alt={gameId.name} className='view-game-container-img-img'/>
+                </div>
+                <div className='view-game-container-text'>
+                    <p className='view-game-year'>{gameId.year}</p>
+                    <p className='view-game-title'>{gameId.title}</p>
+                    <p className='view-game-description'>{gameId.description}</p>
+                </div>
+            
+            </div>
+                
+
         </div>
     )
 }
