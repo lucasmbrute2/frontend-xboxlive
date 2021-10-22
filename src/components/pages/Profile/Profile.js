8af6c0a2fde5f2b29b4c4ba57eece782fc3b40d2
Lucas <lucasmbrute614@gmail.com>
2021-10-22 01:46:06 -0300

    feat(Profile): implement Profile component

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,8 +1,36 @@
-export default function Profile(){
-    return(
-      <div>
+import { useEffect, useState } from "react"
+import { Api } from "../../../Api/Api"
+import "./Profile.scss"
 
-        Profile
+export default function Profile(){
+
+    const [profile,setProfile]= useState([])
+    
+    useEffect(()=>{
+      const getProfile = async ()=>{
+        const response = await Api.getAll('profile',true)
+        const body = await response.json()
+
+        setProfile(body)        
+      } 
+      getProfile()
+    },[profile.id])
+    
+    const HandleJwt = (e)=>{
+      console.log(e)
+    }
+    
+    return(
+      <div className='profile'>
+        {profile.map((eachProfile)=>(
+          <div onClick={()=>HandleJwt(key)} className='profile-container' key={`profile_list_${eachProfile.id}`}>
+            <p className='profile-container-p'>{eachProfile.title}</p>
+            
+            <div className='container-2'>
+              <img className='profile-container-img' src={eachProfile.image} alt={eachProfile.title}/>         
+            </div>        
+          </div>
+        ))}
 
       </div>
     
