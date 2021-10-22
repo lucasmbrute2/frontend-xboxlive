c1975608c6963382cadac843927eac40c548efe4
Lucas <lucasmbrute614@gmail.com>
2021-10-22 20:07:27 -0300

    feat(Profile): implement button to create another profile

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { Api } from "../../../Api/Api"
 import ProfileCard from "../../ProfileCard/ProfileCard"
 import "./Profile.scss"
+import { Link } from "react-router-dom"
 
 export default function Profile(){
 
@@ -16,17 +17,24 @@ export default function Profile(){
       } 
       getProfile()
     },[profile.id])
+    
     return(
-      <div className='profile-card-div'>
-        {profile.map((eachProfile)=>(
-          <ProfileCard 
-          key={`profile_list_${profile.id}`}
-          profile={eachProfile}
-          />
-        ))}
+        <div className='profile-card-div'>
+          {profile.map((eachProfile)=>(
+            <ProfileCard 
+            key={`profile_list_${profile.id}`}
+            profile={eachProfile}
+            />
+          ))}
 
-      </div>
-    
-    
+            <div className='add-profile'>
+              <Link to=''>
+                <i class="fas fa-plus-circle" id='icon-plus'></i>
+              </Link>
+              <p className='add-profile-p'>Adicionar perfil</p>
+            </div>
+            
+        </div>
+
     )
 }
