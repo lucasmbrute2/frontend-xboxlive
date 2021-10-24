bf52c653d6c4651958648ca95b5754b69cfb0a82
Lucas <lucasmbrute614@gmail.com>
2021-10-24 02:44:27 -0300

    feat(Profile): implement ternary logic to render Components in profile

--- src/components/pages/Profile/Profile.js
+++ src/components/pages/Profile/Profile.js
@@ -1,14 +1,28 @@
+import PlusButton from "../../PlusButton/PlusButton"
 import ProfileList from "../../ProfileList/ProfileList"
 import "./Profile.scss"
-
+import { useState } from "react"
+import CreateProfile from "../../CreateProfile/CreateProfile"
 
 export default function Profile(){
 
-
-    return(
-        <div className='profile-card-div'>
-          <ProfileList/> 
+  const [edit,setEdit] = useState(true)
+  
+  return(
+      <div className='profile-card-div'>
+        <div>
+          {edit ? <ProfileList/> : <CreateProfile/>}
         </div>
+        
+        {edit ?
+          <div className='profile-card-div-button'>
+            <i className= "fas fa-plus-circle" onClick={()=>setEdit(!edit)} id='icon-plus'></i>
+            <p className='add-profile-p'>Adicionar perfil</p>
+          </div>
+        : ''
+        }
 
-    )
+      </div>
+
+  )
 }
